43cdffff781fd37ffba49f5e6e8a979088503e6f
paultb3 <paulbenancio3000@gmail.com>
2025-06-03 17:04:47 -0500

    Resuelto conflicto en views.py

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import check_password
+from django.urls import reverse
+from apps.usuarios.models import Usuario
+import uuid
+
+
 def index(request):
-    return HttpResponse("Binevenido al CORE")
+    return render(request, 'core/index.html')
+
+
+def login_view(request):
+    if request.method == "POST":
+        correo = request.POST.get("correo")
+        password = request.POST.get("password")
+
+        try:
+            usuario = Usuario.objects.get(correo=correo)
+            # Si usas contraseña en texto plano (no recomendado), compara así:
+            # if usuario.password == password:
+            # Si usas hash, usa check_password:
+            if check_password(password, usuario.password):
+                # Login exitoso
+                url = reverse(
+                    "comision:bienvenida_comision", kwargs={"usuario_id": usuario.id}
+                )
+                return redirect(url)  # redirige a la página deseada
+            else:
+                error = "Contraseña incorrecta"
+        except Usuario.DoesNotExist:
+            error = "Usuario no encontrado"
+
+        return render(request, "login.html", {"form": {}, "error": error})
+
+    return render(request, "login.html", {"form": {}})
