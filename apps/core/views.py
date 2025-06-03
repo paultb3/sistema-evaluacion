df85d71a4c188e24b2d138939a3069f3e3b2d047
paultb3 <paulbenancio3000@gmail.com>
2025-06-03 15:39:22 -0500

    carga la version estable desl dashboard

--- apps/core/views.py
+++ apps/core/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Binevenido al CORE")
+    return render(request, 'core/index.html')
+
+
+def login(request):
+    return render(request, 'core/login.html')
--- sed/urls.py
+++ sed/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls'))
 ]
