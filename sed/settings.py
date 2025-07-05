32596b0eaf1b9aa130a6db6398bdf9509d2c5069
brenniscastro <brennis.castro@unas.edu.pe>
2025-07-05 06:21:04 -0500

    update

--- sed/settings.py
+++ sed/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["sed.pautb.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
